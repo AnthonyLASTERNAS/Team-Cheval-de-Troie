655bfcce6a4d83fc7748b918f0b658b9014a789e
LASTERNAS <126486460+AnthonyLASTERNAS@users.noreply.github.com>
2023-05-11 23:26:32 +0200

    Revert "Logo kyx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { Link } from "react-router-dom";
-import { ReactPropTypes } from "react";
-import "./App.css";
-import fond from './assets/fond.jpg';
-import logo from './assets/logo.png';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const style = {
-    backgroundImage: `url(${fond})`,
-    height: '100vh',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  };
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App" style={style}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <>
-        <h1>Cheval de Troie</h1>
-        <h2>Incite toi à prendre des vacances</h2>
-        <div>
-          <span></span>
-        </div>
-        <h2>DECOUVRE OU TU PARS EN VACANCES (forcés)*</h2>
-        <div></div>
-        <h5>*Offert généreusement par les formateurs</h5>
-      </>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
